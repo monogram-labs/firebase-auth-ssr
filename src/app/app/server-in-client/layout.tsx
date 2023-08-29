db510afa0c8695efc6084e86124b811d6cc30786
Leonardo Ortiz <leo@monogram.io>
2023-08-29 11:05:27 -0300

    remove count from layout

--- src/app/app/server-in-client/layout.tsx
+++ src/app/app/server-in-client/layout.tsx
@@ -1,14 +1,5 @@
 'use client'
 
-import { useReducer } from 'react'
-
 export default function Layout({ children }: { children: JSX.Element }) {
-	const [count, increment] = useReducer((count) => count + 1, 0)
-
-	return (
-		<main>
-			<button onClick={increment}>client layout wrapper, count is {count}</button>
-			{children}
-		</main>
-	)
+	return <main>{children}</main>
 }
